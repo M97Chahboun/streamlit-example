9ca1e6d0ac5bf160d883620ad51b8e12ccfc85a2
CHAHBOUN Mohammed <69054810+M97Chahboun@users.noreply.github.com>
2022-05-18 15:39:16 +0100

    test annotated text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,52 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+from annotated_text import annotated_text, annotation
+
 
 """
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+# Annotated text example
+Below is some annotated text:
 """
 
+with st.echo():
+    annotated_text(
+        "This ",
+        ("is", "verb"),
+        " some ",
+        ("annotated", "adj"),
+        ("text", "noun"),
+        " for those of ",
+        ("you", "pronoun"),
+        " who ",
+        ("like", "verb"),
+        " this sort of ",
+        ("thing", "noun"),
+        "."
+    )
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral test", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral test", 1, 100, 9)
+# Weird, right now there's negative margin at the bottom of all st.markdown.
+# Adding this to make up for it, but we should fix it in Streamlit itself.
+""
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+"""
+Bam!
+"""
 
-    points_per_turn = total_points / num_turns
+"""
+And you can also customize colors:
+"""
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+with st.echo():
+    annotated_text(
+        "This ",
+        ("is", "verb", "#8ef"),
+        " some ",
+        ("annotated", "adj", "#faa"),
+        ("text", "noun", "#afa"),
+        " for those of ",
+        ("you", "pronoun", "#fea"),
+        " who ",
+        ("like", "verb", "#8ef"),
+        " this sort of ",
+        ("thing", "noun", "#afa"),
+        "."
+    )
