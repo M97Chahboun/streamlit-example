1b555d2cc5c95ad404b3376e169e1418be7a78af
CHAHBOUN Mohammed <69054810+M97Chahboun@users.noreply.github.com>
2022-05-24 10:38:54 +0100

    test table

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,52 +1,111 @@
+import pandas as pd
 import streamlit as st
-from annotated_text import annotated_text, annotation
+from st_aggrid import AgGrid, GridOptionsBuilder
+from st_aggrid.shared import GridUpdateMode
+
+STREAMLIT_AGGRID_URL = "https://github.com/PablocFonseca/streamlit-aggrid"
+st.set_page_config(
+    layout="centered", page_icon="🖱️", page_title="Interactive table app"
+)
+st.title("🖱️ Interactive table app")
+st.write(
+    """This app shows how you can use the [streamlit-aggrid](STREAMLIT_AGGRID_URL) 
+    Streamlit component in an interactive way so as to display additional content 
+    based on user click."""
+)
 
 
-"""
-# Annotated text example
-Below is some annotated text:
-"""
+st.write("Go ahead, click on a row in the table below!")
 
-with st.echo():
-    annotated_text(
-        "This ",
-        ("is", "verb"),
-        " some ",
-        ("annotated", "adj"),
-        ("text", "noun"),
-        " for those of ",
-        ("you", "pronoun"),
-        " who ",
-        ("like", "verb"),
-        " this sort of ",
-        ("thing", "noun"),
-        "."
+
+def aggrid_interactive_table(df: pd.DataFrame):
+    """Creates an st-aggrid interactive table based on a dataframe.
+
+    Args:
+        df (pd.DataFrame]): Source dataframe
+
+    Returns:
+        dict: The selected row
+    """
+    options = GridOptionsBuilder.from_dataframe(
+        df, enableRowGroup=True, enableValue=True, enablePivot=True
     )
 
-# Weird, right now there's negative margin at the bottom of all st.markdown.
-# Adding this to make up for it, but we should fix it in Streamlit itself.
-""
+    options.configure_side_bar()
 
-"""
-Bam!
-"""
-
-"""
-And you can also customize colors:
-"""
-
-with st.echo():
-    annotated_text(
-        "This ",
-        ("is", "verb", "#8ef"),
-        " some ",
-        ("annotated", "adj", "#faa"),
-        ("text", "noun", "#afa"),
-        " for those of ",
-        ("you", "pronoun", "#fea"),
-        " who ",
-        ("like", "verb", "#8ef"),
-        " this sort of ",
-        ("thing", "noun", "#afa"),
-        "."
+    options.configure_selection("single")
+    selection = AgGrid(
+        df,
+        enable_enterprise_modules=True,
+        gridOptions=options.build(),
+        theme="light",
+        update_mode=GridUpdateMode.MODEL_CHANGED,
+        allow_unsafe_jscode=True,
     )
+
+    return selection
+
+
+iris = pd.read_csv(
+    "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
+)
+
+selection = aggrid_interactive_table(df=iris)
+
+if selection:
+    st.write("You selected:")
+    st.json(selection["selected_rows"])
+
+st.write("## Code")
+
+st.code(
+    '''
+import pandas as pd
+import streamlit as st
+from st_aggrid import AgGrid, GridOptionsBuilder
+from st_aggrid.shared import GridUpdateMode
+
+iris = pd.read_csv(
+    "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
+)
+
+def aggrid_interactive_table(df: pd.DataFrame):
+    """Creates an st-aggrid interactive table based on a dataframe.
+
+    Args:
+        df (pd.DataFrame]): Source dataframe
+
+    Returns:
+        dict: The selected row
+    """
+    options = GridOptionsBuilder.from_dataframe(
+        df, enableRowGroup=True, enableValue=True, enablePivot=True
+    )
+
+    options.configure_side_bar()
+
+    options.configure_selection("single")
+    selection = AgGrid(
+        df,
+        enable_enterprise_modules=True,
+        gridOptions=options.build(),
+        theme="light",
+        update_mode=GridUpdateMode.MODEL_CHANGED,
+        allow_unsafe_jscode=True,
+    )
+
+    return selection
+
+
+iris = pd.read_csv(
+    "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
+)
+
+selection = aggrid_interactive_table(df=iris)
+
+if selection:
+    st.write("You selected:")
+    st.json(selection["selected_rows"])
+''',
+    "python",
+)
